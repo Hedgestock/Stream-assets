69e5397186f50633a266087d65eb9baa5f34b2db
Hedgestock <pamarthur.aito@gmail.com>
2021-11-13 09:23:02 +0100

    Added RDC overlay

---
+++ RDC/main.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("onWidgetLoad", function (obj) {
+  document.getElementById("player1-score").innerText = "{player1score}";
+  document.getElementById("player2-score").innerText = "{player2score}";
+  document.getElementById("player1-name").innerText = "{player1name}";
+  document.getElementById("player2-name").innerText = "{player2name}";
+  document.getElementById("round").innerText = "{round}";
+});
